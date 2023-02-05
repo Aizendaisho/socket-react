831abbbbabaf36c97c8af2ef6e9d1ec5cdaacb6f
Aizendaisho <96481537+Aizendaisho@users.noreply.github.com>
2023-02-05 16:37:41 -0400

    conexion de socket con el back y front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
+import io from "socket.io-client";
 
+const socket = io("http://localhost:8000");
+const App = () => {
+  return <div className="">App</div>;
+};
 
-const App =() =>{
-  return (
-    <div className="" >App</div>
-  )
-}
-
-export default App
+export default App;
